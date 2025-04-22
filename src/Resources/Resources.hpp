213632fec7c31b56122a605d8c3209e6ada76664
Chrohen <bykov.03.aa@gmail.com>
2025-04-22 22:26:30 +0300

    Linking Fixes
    Fixed linking in project setting. Also, replaced old SFML files in project folder (it solved some errors, maybe missing files due to gitignore)

--- src/Resources/Resources.hpp
+++ src/Resources/Resources.hpp
@@ -1,7 +1,7 @@
 #ifndef RESOURCES
 #define RESOURCES
 
-#include <SFML/Graphics/Texture.hpp>
+#include "SFML/Graphics/Texture.hpp"
 
 constexpr int TEXTURE_SIZE = 16, ROW_WALL_TEXT_COUNT = 6, ROW_SHADER_TEXT_COUNT = 6, 
 ROW_FLOOR_TEXT_COUNT = 3, EMPTY_CELL_CODE = -1;
