21093f3abe45a518655b94dd4e32df3a34dc668b
Chrohen <bykov.03.aa@gmail.com>
2025-04-25 10:34:39 +0300

    Changes in PlayerController

--- src/entites/controllers/PlayerController.hpp
+++ src/entites/controllers/PlayerController.hpp
@@ -5,12 +5,9 @@
 #include "Controller.hpp"
 
 class PlayerController : public Controller{
+	std::unique_ptr<View> GetView();
 	virtual bool CheckCollision(sf::Vector2f position) override;
 	virtual void Init(std::vector<SpMap>) override;
-class PlayerController {
-	void Init(std::vector<SpMap>);
-	std::unique_ptr<View> GetView();
-	Player GetPlayer();
 };
 
 #endif // !PLAYER_CONTROLLER
